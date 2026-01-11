934548bf34870bc39357a11484e81c05cf01b93c
Gabriella Miesner <51331370+gabriellamiesner@users.noreply.github.com>
2026-01-11 08:49:26 -0500

    pt 2 initial commit

---
+++ pt1/binary_search.py
@@ -0,0 +1,20 @@
+def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0 
+        r = len(nums) -1
+
+        while l <=r: 
+            middle = (l+r)//2
+
+            if nums[middle] == target: 
+                return middle 
+            elif nums[middle] > target: 
+                r = middle -1
+            else: 
+                l = middle + 1
+        return -1
+        
