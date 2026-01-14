96cc64d478908e50be95eea2c8f631fbc8c111a3
Gabriella Miesner <51331370+gabriellamiesner@users.noreply.github.com>
2026-01-14 17:29:47 -0500

    Create search_2d_matrix.py

---
+++ pt3/search_2d_matrix.py
@@ -0,0 +1,37 @@
+def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        top = 0 
+        bottom = rows -1
+
+        while top <= bottom: 
+            row = (top + bottom) //2
+            if target > matrix[row][-1]:
+                top = row +1
+            elif target < matrix[row][0]: 
+                bottom = row -1
+            else: 
+                break 
+        
+        if not (top <= bottom):
+            return False 
+
+        row = (top + bottom) //2
+        l = 0
+        r = cols -1
+        while l <= r: 
+            middle = (l +r) //2
+            if target > matrix[row][middle]: 
+                l = middle +1
+            elif target < matrix[row][middle]: 
+                r = middle -1
+            else: 
+                return True
+        return False 
