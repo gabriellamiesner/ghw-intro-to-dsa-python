ff145d2247797c6ea2671653370ff2ccf88cbf3e
Gabriella Miesner <51331370+gabriellamiesner@users.noreply.github.com>
2026-01-14 17:30:42 -0500

    Create ll_cycle.py

---
+++ pt3/ll_cycle.py
@@ -0,0 +1,16 @@
+ def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        fast = head 
+        slow = head 
+
+        while fast and fast.next: 
+            fast = fast.next.next 
+            slow = slow.next 
+            if fast == slow: 
+                return True 
+        return False
+        
